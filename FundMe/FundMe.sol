c33ff35506c3e63907f78848091df86f336f7a3d
KleanthisLiontis <kleanthisliontis@gmail.com>
2023-12-19 22:47:40 +0200

    asd

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -11,32 +11,39 @@ import {PriceConverter} from "./PriceConverter.sol";
 //Customer errors
 error NotOwner();
 
-contract FundMe{
+contract FundMe {
     using PriceConverter for uint256;
 
     //constant is used for compile time var declaration
     uint256 public constant MINIMUM_USD = 5e18;
     address[] public funders;
-    mapping(address => uint256 amountFunded)public addressToAmountFunded;
+    mapping(address => uint256 amountFunded) public addressToAmountFunded;
 
     //Immutable is set once and then cannot change.
     address public immutable i_owner;
+
     //auto called when contract is created on chain.
     constructor() {
         i_owner = msg.sender;
     }
 
-    function fund() public payable{
-
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
         //require(getConversionRate(msg.value) >= minimumUSD,"Send more money pleaze :)");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public OnlyOwner{
+    function withdraw() public OnlyOwner {
         //require(msg.sender == owner,"Must be owner");
-        for(uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
@@ -52,21 +59,26 @@ contract FundMe{
         require (sendSuccess,"Send Failed");
         */
         //call
-        (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}("");
-        require (callSuccess,"Call Failed");
+        (bool callSuccess, bytes memory dataReturned) = payable(msg.sender)
+            .call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
     }
 
-    function getPrice() public view returns(uint256){
+    function getPrice() public view returns (uint256) {
         //Address - 0x694AA1769357215DE4FAC081bf1f309aDC325306
         //ABI - Interface of contract need to know what functions we can use not how they are implemented
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int price,,,) = priceFeed.latestRoundData();
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int price, , , ) = priceFeed.latestRoundData();
         //Price of eth in terms of USD
         //Number will be without decimals since Sol doesnt handle them well.
         return uint256(price * 1e10);
     }
-    
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+
+    function getConversionRate(
+        uint256 ethAmount
+    ) public view returns (uint256) {
         // 1 ETH?
         // 2000_000000000000000000
         uint256 ethPrice = getPrice();
@@ -74,10 +86,13 @@ contract FundMe{
         uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
         return ethAmountInUSD;
     }
+
     //Adds this code to start of function and then rest of code _; position matters
-    modifier OnlyOwner(){
+    modifier OnlyOwner() {
         //require(require(msg.sender == i_owner,"Must be owner"));
-        if(msg.sender != i_owner) {revert NotOwner();}
+        if (msg.sender != i_owner) {
+            revert NotOwner();
+        }
         _;
     }
 

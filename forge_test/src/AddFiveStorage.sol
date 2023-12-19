771fa5cfd17e3a87b2e11f6026c4c360132f5301
Kleanthis Liontis <kleanthisliontis@gmail.com>
2023-12-19 23:50:47 +0200

    thjis  will be a mess

---
+++ forge_test/src/AddFiveStorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+//Child of SimpleStorage
+contract AddFiveStorage is SimpleStorage {
+
+    //Also need to change in parent contract add virtual to function.
+    function store(uint256 _new_number) public override{
+        my_favourite_number = _new_number +5;
+    }
+}

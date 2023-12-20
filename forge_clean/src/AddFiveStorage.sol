b57e36366a61d609d220c1c35789dd1458fa2377
Kleanthis Liontis <kleanthisliontis@gmail.com>
2023-12-20 10:32:14 +0200

    deployed to testnet

--- forge_clean/src/AddFiveStorage.sol
+++ forge_clean/src/AddFiveStorage.sol
@@ -5,9 +5,8 @@ import {SimpleStorage} from "./SimpleStorage.sol";
 
 //Child of SimpleStorage
 contract AddFiveStorage is SimpleStorage {
-
     //Also need to change in parent contract add virtual to function.
-    function store(uint256 _new_number) public override{
-        my_favourite_number = _new_number +5;
+    function store(uint256 _new_number) public override {
+        my_favourite_number = _new_number + 5;
     }
 }
